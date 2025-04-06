869d5e2658fd0547e911cf1402225a753818651e
Muzayzaymu <muzayzaymu@gmail.com>
2025-04-07 00:09:34 +0500

    homework spring 2

---
+++ src/main/java/org/skypro/skyshop/service/BasketService.java
@@ -0,0 +1,46 @@
+package org.skypro.skyshop.service;
+
+import org.skypro.skyshop.model.UserBasket;
+import org.skypro.skyshop.model.basket.BasketItem;
+import org.skypro.skyshop.model.basket.ProductBasket;
+import org.skypro.skyshop.model.product.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class BasketService {
+
+    private final ProductBasket productBasket;
+    private final StorageService storageService;
+
+    public BasketService(ProductBasket productBasket, StorageService storageService) {
+        this.productBasket = productBasket;
+        this.storageService = storageService;
+    }
+
+    public void addProductToBasket(UUID id) {
+        Product product = storageService.getProductById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Product with id " + id + " not found"));
+        productBasket.addProduct(id);
+    }
+
+    public UserBasket getUserBasket() {
+        List<BasketItem> basketItems = productBasket.getProducts().entrySet().stream()
+                .map(entry -> {
+                    UUID productId = entry.getKey();
+                    int quantity = entry.getValue();
+                    Product product = storageService.getProductById(productId).orElseThrow(() -> new IllegalArgumentException("Product not found: " + productId));
+                    return new BasketItem(product, quantity);
+                })
+                .collect(Collectors.toList());
+
+        double total = basketItems.stream()
+                .mapToDouble(item -> item.getProduct().getCost() * item.getQuantity())
+                .sum();
+
+        return new UserBasket(basketItems, total);
+    }
+}
